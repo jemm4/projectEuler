91ccb60cc7458ad8f5f653fb54db4e6dd72fe20e
STA Students <martinez.jeffrey@ymail.com>
2017-08-18 10:59:14 -0400

    Added working files

---
+++ Euler3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package euler1;
+
+/**
+ *
+ * @author stastudents
+ */
+public class Euler3 {
+    public static void main(String[] args) {
+        /* 
+         * The prime factors of 13195 are 5, 7, 13, 29. 
+         * What is the largest prime factor of the number 600851475143?
+         */
+        
+        // declare variables
+//        long num = 600851475143;
+    }
+        
+
+}
---
+++ Euler4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package euler1;
+
+/**
+ *
+ * @author stastudents
+ */
+public class Euler4 {
+    /* 
+     * A palindromic number reads the same both ways. The largest palindrome 
+     * made from the product of two 2-digit numbers is 9009 = 91 × 99.
+     * Find the largest palindrome made from the product of two 3-digit numbers.
+    */
+    // declare variables
+    
+    
+}
